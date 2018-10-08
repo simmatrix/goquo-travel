0b10e10e75909f68c1eb71584fa82d291a436a32
Vince Wong <laisim@netccentric.com>
2018-10-08 09:44:13 +0800

    Updates

---
+++ precache-manifest.a06c99452bf8a1591106862febf1e665.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "dba6692a9a2caea7a16522cbf1968b91",
+    "url": "/goquo-travel/static/media/HelveticaNeueUltraLight.dba6692a.woff"
+  },
+  {
+    "revision": "4fb18e5aadf96b108cac",
+    "url": "/goquo-travel/static/css/main.e71f797f.chunk.css"
+  },
+  {
+    "revision": "9e416d0e028b2225db0ceac533f02bb0",
+    "url": "/goquo-travel/static/media/logo.9e416d0e.svg"
+  },
+  {
+    "revision": "4a9e97f01412c2a4ab16",
+    "url": "/goquo-travel/static/js/1.4a9e97f0.chunk.js"
+  },
+  {
+    "revision": "9d992a91cfa818c72559",
+    "url": "/goquo-travel/static/js/runtime~main.9d992a91.js"
+  },
+  {
+    "revision": "61022fffec537c1eb4ef90f57cbdec5c",
+    "url": "/goquo-travel/static/media/header-image.61022fff.png"
+  },
+  {
+    "revision": "fc15767a62a8f7f7650ffdeae4db4b28",
+    "url": "/goquo-travel/static/media/header-background.fc15767a.jpg"
+  },
+  {
+    "revision": "885e502feacfe7a2244b3f1fc232456e",
+    "url": "/goquo-travel/static/media/HelveticaNeueUltraLightItal.885e502f.woff"
+  },
+  {
+    "revision": "912504a26e60acf7cf14b96eab5cca93",
+    "url": "/goquo-travel/static/media/HelveticaNeueItalic.912504a2.woff"
+  },
+  {
+    "revision": "4fb18e5aadf96b108cac",
+    "url": "/goquo-travel/static/js/main.4fb18e5a.chunk.js"
+  },
+  {
+    "revision": "da0396e9e5b99bb4f38cdbd3ac4f8691",
+    "url": "/goquo-travel/static/media/HelveticaNeueLightItalic.da0396e9.woff"
+  },
+  {
+    "revision": "9a0b29b6d9d3a9d3e74ce03bed375aa4",
+    "url": "/goquo-travel/static/media/HelveticaNeueBold.9a0b29b6.woff"
+  },
+  {
+    "revision": "61bce9764944398be6d90a27fabc9c58",
+    "url": "/goquo-travel/static/media/HelveticaNeueLight.61bce976.woff"
+  },
+  {
+    "revision": "9b62599c2a012d8d204f503dd75a58b1",
+    "url": "/goquo-travel/static/media/HelveticaNeueCondensedBold.9b62599c.woff"
+  },
+  {
+    "revision": "789669526b13f9ec76eb437ac346269a",
+    "url": "/goquo-travel/static/media/HelveticaNeueBoldItalic.78966952.woff"
+  },
+  {
+    "revision": "e79d51f81604eba0e771138aee67c53a",
+    "url": "/goquo-travel/static/media/HelveticaNeueCondensedBlack.e79d51f8.woff"
+  },
+  {
+    "revision": "4a9e97f01412c2a4ab16",
+    "url": "/goquo-travel/static/css/1.8ca5bdb1.chunk.css"
+  },
+  {
+    "revision": "c923e09dad14828c1b43dee81bfcb280",
+    "url": "/goquo-travel/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/goquo-travel/precache-manifest.a06c99452bf8a1591106862febf1e665.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/goquo-travel/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
